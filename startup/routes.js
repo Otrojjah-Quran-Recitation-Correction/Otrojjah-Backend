2e4541989fe27cec6e2aef761ff29ccf50d32c5f
Mohamed <mohamedmahmoudalihassan@gmail.com>
2020-03-09 18:00:43 +0200

    Get rule's children by parent id

--- startup/routes.js
+++ startup/routes.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 const error = require("../middleware/error");
 const users = require("../routes/users");
 const records = require("../routes/records");
-const hokm = require("../routes/hokm");
+const rule = require("../routes/rule");
 const client = require("../routes/client");
 const shaikh = require("../routes/shaikh");
 const label = require("../routes/label");
@@ -16,7 +16,7 @@ module.exports = function(app) {
   app.use(express.json());
   app.use("/api/users", users);
   app.use("/api/records", records);
-  app.use("/api/hokm", hokm);
+  app.use("/api/rule", rule);
   app.use("/api/client", client);
   app.use("/api/shaikh", shaikh);
   app.use("/api/label", label);
