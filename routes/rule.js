0ac80f197d833553b81d2c5506bd4a221616793f
Mohamed <mohamedmahmoudalihassan@gmail.com>
2020-03-10 05:17:24 +0200

    Rule route put operation

--- routes/rule.js
+++ routes/rule.js
@@ -4,13 +4,14 @@ const Joi = require("joi");
 const _ = require("lodash");
 const { Rule, getRuleChildren, validateRule } = require("../models/rule");
 
+router.get("/", async (req, res) => {
+  const rules = await getRuleChildren();
+  if (!rules) return res.status(400).send("There is no root rule.");
+  res.send(rules);
+});
+
 router.get("/:id", async (req, res) => {
   const { id } = req.params;
-
-  if (id) {
-    const { error } = Joi.validate({ id }, { id: Joi.objectId() });
-    if (error) return res.status(400).send(error.details[0].message);
-  }
 
   const rules = await getRuleChildren(id);
   if (!rules) return res.status(400).send("There is no children of this rule.");
@@ -22,52 +23,40 @@ router.post("/", async (req, res) => {
   const { error } = validateRule(req.body);
   if (error) return res.status(400).send(error.details[0].message);
 
-  let rule = await Rule.findOne({ name: req.body.name });
+  let rule = await Rule.findOne({
+    name: req.body.name,
+    parentId: req.body.parentId
+  });
   if (rule) return res.status(400).send("Rule is already exists.");
 
-  const rootRule = await getRuleChildren();
-  if (rootRule && !rule.parentId)
-    return res.status(400).send("There is already root rule.");
+  if (!req.body.parentId) {
+    const rootRule = await getRuleChildren();
+    if (rootRule) return res.status(400).send("There is already root rule.");
+  }
 
-  rule = new Rule(_.pick(req.body, ["name", "description", "parentId"]));
+  rule = new Rule(req.body);
   await rule.save();
 
   res.send(rule);
 });
 
-// router.put("/:id", async (req, res) => {
-//   const { id } = req.params;
+router.put("/:id", async (req, res) => {
+  const { id } = req.params;
 
-//   const { error: idError } = Joi.validate({ id }, { id: Joi.objectId() });
-//   if (idError) return res.status(400).send(idError.details[0].message);
+  const { error } = validateRule(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
 
-//   const { error } = validateHokm(req.body);
-//   if (error) return res.status(400).send(error.details[0].message);
+  if (req.body.parentId)
+    return res.status(400).send("ParentId can't be modified.");
 
-//   const hokm = await Hokm.findById(id);
-//   if (!hokm) return res.status(400).send("There is no such record");
+  const Rule = await Rule.findById(id);
+  if (!Rule) return res.status(400).send("There is no such rule");
 
-//   const newHokm = await Hokm.findByIdAndUpdate(
-//     id,
-//     _.pick(req.body, ["name", "details"]),
-//     {
-//       new: true
-//     }
-//   );
+  const newRule = await Rule.findByIdAndUpdate(id, req.body, {
+    new: true
+  });
 
-//   res.send(newHokm);
-// });
-
-// router.delete("/:id", async (req, res) => {
-//   const { id } = req.params;
-
-//   const { error: idError } = Joi.validate({ id }, { id: Joi.objectId() });
-//   if (idError) return res.status(400).send(idError.details[0].message);
-
-//   const hokm = await Hokm.findByIdAndRemove(id);
-//   if (!hokm) return res.status(400).send("There is no such record");
-
-//   res.send(hokm);
-// });
+  res.send(newRule);
+});
 
 module.exports = router;
