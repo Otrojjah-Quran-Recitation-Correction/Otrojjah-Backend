147bbc9572eb046afed5526e82a95d4cd7e2c6f6
Mohamed <mohamedmahmoudalihassan@gmail.com>
2020-03-10 21:21:14 +0200

    Reformat middlewares(removing comments :D )

--- middleware/error.js
+++ middleware/error.js
@@ -2,13 +2,6 @@ const winston = require("winston");
 
 module.exports = function(err, req, res, next) {
   winston.error(err.message, err);
-  //todo adding a try catch for the worngly sent post request (parsing error)
-  // error
-  // warn
-  // info
-  // verbose
-  // debug
-  // silly
 
   res.status(500).send("Something failed.");
 };
--- middleware/shaikh.js
+++ middleware/shaikh.js
@@ -1,7 +1,4 @@
 module.exports = function(req, res, next) {
-  // 401 Unauthorized
-  // 403 Forbidden
-
   if (!req.user.isShaikh)
     return res.status(403).send("Access denied. Not a Shaikh");
 
