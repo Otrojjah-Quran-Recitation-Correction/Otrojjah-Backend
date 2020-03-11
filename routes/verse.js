f6ad208c489e828e8402c85fe66024cba47524bb
Mohamed <mohamedmahmoudalihassan@gmail.com>
2020-03-11 04:38:59 +0200

    Verse route - put and delete operations added

--- routes/verse.js
+++ routes/verse.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const Joi = require("joi");
 const _ = require("lodash");
 const { Verse, validateVerse, getVerses } = require("../models/verse");
+const validateObjectId = require("../middleware/validateObjectId");
 
 router.get("/", async (req, res) => {
   const verses = await getVerses(req.query);
@@ -18,6 +19,26 @@ router.post("/", async (req, res) => {
 
   verse = new Verse(req.body);
   await verse.save();
+
+  res.send(verse);
+});
+
+router.put("/:id", validateObjectId, async (req, res) => {
+  const { error } = validateVerse(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  const verse = await Verse.findByIdAndUpdate(req.params.id, req.body, {
+    new: true
+  });
+  if (!verse) return res.status(400).send("Verse is not found.");
+
+  res.send(verse);
+});
+
+router.delete("/:id", validateObjectId, async (req, res) => {
+  const verse = await Verse.findByIdAndRemove(req.params.id);
+
+  if (!verse) return res.status(404).send("Verse is not found.");
 
   res.send(verse);
 });
