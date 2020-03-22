7e3daf5c2791c2af9dcc73ee27cb7f8d2749f2e4
Mohamed <mohamedmahmoudalihassan@gmail.com>
2020-03-22 13:36:51 +0200

    empty commit

--- credentialsSetup.js
+++ credentialsSetup.js
@@ -1,10 +1,5 @@
 const fs = require("fs");
 
-consol.log(
-  process.env.GOOGLE_APPLICATION_CREDENTIALS,
-  process.env.GOOGLE_CREDENTIALS
-);
-
 fs.writeFile(
   process.env.GOOGLE_APPLICATION_CREDENTIALS,
   process.env.GOOGLE_CREDENTIALS,
