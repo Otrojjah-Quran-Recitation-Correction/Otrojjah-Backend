2d64c366aa0ff76176b3c3221c4b5efefce84fd0
Mohamed <mohamedmahmoudalihassan@gmail.com>
2020-03-22 12:38:17 +0200

    process.env insted of config in credentials setup

--- credentialsSetup.js
+++ credentialsSetup.js
@@ -1,8 +1,7 @@
 const fs = require("fs");
-const config = require("config");
 
 fs.writeFile(
-  config.get("GOOGLE_CREDENTIALS"),
-  config.get("GOOGLE_APPLICATION_CREDENTIALS"),
+  process.env.GOOGLE_CREDENTIALS,
+  process.env.GOOGLE_APPLICATION_CREDENTIALS,
   err => {}
 );
