bf73331726be5ffb52e05c3907e6a9acc50f8479
Mohamed <mohamedmahmoudalihassan@gmail.com>
2020-03-22 05:43:09 +0200

    Heroku testing

--- credentialsSetup.js
+++ credentialsSetup.js
@@ -1,5 +1,10 @@
 const fs = require("fs");
 
+console.log(
+  process.env.storage_credentials,
+  process.env.storage_credentials_file
+);
+
 fs.writeFile(
   process.env.storage_credentials,
   process.env.storage_credentials_file,
