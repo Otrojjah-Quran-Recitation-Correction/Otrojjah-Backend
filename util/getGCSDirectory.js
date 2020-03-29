9934c57be486ece7c40cc5b8ec6e6ed143ba0dea
Mohamed <mohamedmahmoudalihassan@gmail.com>
2020-03-29 15:46:06 +0200

    Shaikh and client records are separated

--- util/getGCSDirectory.js
+++ util/getGCSDirectory.js
@@ -1,6 +1,8 @@
 module.exports = function(reqBody, file) {
+  const type = reqBody.isShaikh ? "Shaikh" : "Client";
+
   if (file.originalname === "blob") file.originalname = `${Date.now()}.wav`;
   const fileName = file.originalname.toString().replace(/\s+/g, "-");
 
-  return `${reqBody.verseId}/${fileName}`;
+  return `${type}/${reqBody.verseId}/${fileName}`;
 };
